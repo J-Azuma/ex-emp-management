0f9ba4490d1a04424871817edeee204772e2ea01
J-azuma <azumanotdetail@gmail.com>
2020-06-18 09:56:50 +0900

    modify EmployeeService : delete unnecessary import

--- src/main/java/jp/co/sample/service/EmployeeService.java
+++ src/main/java/jp/co/sample/service/EmployeeService.java
@@ -2,9 +2,8 @@ package jp.co.sample.service;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
